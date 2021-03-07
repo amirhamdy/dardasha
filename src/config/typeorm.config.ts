ac8d0f1a6029d761076263dbe28e38b1b7c017a5
Amir <amirhamdy4@gmail.com>
2021-03-07 20:32:51 +0200

    doing it live

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -2,6 +2,17 @@ import { TypeOrmModuleOptions } from "@nestjs/typeorm";
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
   type: "mysql",
+  host: "fdb16.runhosting.com",
+  port: 3306,
+  username: "2459372_db",
+  password: "395iudus4yJ0gi7N",
+  database: "2459372_db",
+  entities: ["dist/**/*.entity{.ts,.js}"],
+  synchronize: true
+};
+
+export const typeOrmConfigLocal: TypeOrmModuleOptions = {
+  type: "mysql",
   host: "localhost",
   port: 3306,
   username: "root",
