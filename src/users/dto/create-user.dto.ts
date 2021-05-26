9bb22878d9db44cc4357fb87b074b087f0776861
Amir Hamdy <amirhamdy4@gmail.com>
2021-05-26 21:48:22 +0200

    add more info about the user

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -6,4 +6,18 @@ export class CreateUserDto {
   @IsNotEmpty()
   @ApiProperty({ example: "Amir Hamdy", description: "The full-name of the user" })
   name: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty({ example: "amirhamdy4@gmail.com", description: "The email of the user" })
+  email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty({ example: "123456789", description: "The password of the user" })
+  password: string;
+
+  @IsString()
+  @ApiProperty({ description: "The device token of the user" })
+  token: string;
 }
--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -10,4 +10,15 @@ export class User extends BaseEntity {
   @Column()
   @ApiProperty({ example: "Amir Hamdy", description: "The full-name of the user" })
   name: string;
+
+  @Column()
+  @ApiProperty({ example: "amirhamdy4@gmail.com", description: "The email of the user" })
+  email: string;
+
+  @Column()
+  @ApiProperty({ example: "123456789", description: "The password of the user" })
+  password: string;
+
+  @Column()
+  token: string;
 }
